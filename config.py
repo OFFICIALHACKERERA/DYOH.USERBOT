8a0ee3d1c1f5045b8b70bc4fc043618e9f999dad
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2024-10-26 18:23:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+# Telegram API configuration
+APP_ID = int(os.environ.get("APP_ID", 22030138))
+API_HASH = os.environ.get("API_HASH", "c6c02e51a03f6b03dba9ad9a826dc2f1")
+
+# Define string sessions
+STRING_SESSION = [
+    os.environ.get("STRINGS_SESSION_1", "1BVtsOJkBu6zMqekR3w3geIAq7TiyjJ2hC7rExNw6_DmaXoJwi20BJVabpCvDVbGq8AN8YU75CPzAype5HMd0shjj_wvO_3aENCY51kb9Jw9uEn0bBUfgo4oK_vQf7uqt5jJ7e_huFgfIPArA-SDIItAP2wc5wS6QV4lj-rjnyxtF8XZHayh4NrAwdhleoAoZZyjHyhTHO1g4tj5xqUii1_1BuZI40yRwV_wgWw2cpnJaVgCCNJAPQ8vihT_COQYN4KCZXr_Z2cZhVI0Il8G2yPzFPoYR_R-oxkYqX4xgkIzHHIoHU_NSNC8GPAf8rVil_vmovcRESZ8U4QPM8CJFEytavDfV9Gc="),
+    os.environ.get("STRINGS_SESSION_2", "1BVtsOGcBuzTNIJm8ak4Xnl7FDovlhgUxbOZIjVse38Vs--WWe2J0OXnfLL63Z0GQiyZX1xx6DnwcrqaR_dslw4vqXt6gZtt2hdgChIbD2eDoAbJcHZNKUSGFGFAcv9BeuiCZd-fTZOCmDP41EWpuhn5EpKbNr3DY9mw9dtSoEvqYqxp8XGQCIko3B-Tr63aMNf0PVDArZcDmyAuDrY0C0rc-I5-m9_n8MbWT78vg6ANB2nWP95yQorqruf9BPiFJF8t36_qhyE0TXO58Y2O93D3JQV-UMIUqJDVPy1ABWqrqae54mfbhHeBOgJTb3tGfS16nq3ygwxMEbIJ2GAduDVfvEdaNXio=")
+]
+
+# User and Bot Configuration
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "@YourBotUsername")
+ALLOWED_USER_ID = 6508157803  # Replace with your actual user ID
+OWNER_ID = "raoxc"  # Owner ID or username
+CHAT_ID = '-1002217780167'
+
+
+# Media and Version Info
+IPIC = "https://telegra.ph/file/6bb3994d5789d8e7f2c99.mp4"  # URL for the media file
+ubotversion = "1.0"  # Version of your userbot
+
+
