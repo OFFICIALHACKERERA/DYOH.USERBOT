963c8b75de4241ddd3fe3ba666fc554dfecc7d50
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2024-10-27 00:17:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 APP_ID = int(os.environ.get("APP_ID", 22030138))
 API_HASH = os.environ.get("API_HASH", "c6c02e51a03f6b03dba9ad9a826dc2f1")
 
+ALLOWED_USER_ID = 6437064649  # Replace with your actual user ID
 # Define string sessions
 STRING_SESSION = [
     os.environ.get("STRINGS_SESSION_1", "1BVtsOLoBu2_ICyYCk7K8luN1-RI1Rn2LgTQopwY2-6wOMJ756-ScQUXV1Jbe9wwf2CrDIjQGsXH4QJGyJ_CXYBP-_XV0o02n6PoHtnR3wKuqJWJYZMy0n3xl876wxD88HSu0uHv-nkDSCVlEfUUoDD7wwZIOmQlnvNUQoNwCl1fCuYAZcE2XWn_qYyF38aMF21fAj3tAlm6fK5O5Gn4ZL0wsyOJsWVqFUaaupV2jTyYivm8h1eTgbJvaN2fA1ZkdTAM8_K2e5CyUtU_MUUNWoRshHTucxavmz0RLseaw4cjpzc8oKhuOuhq_PxoHKsm4xiDrgbL3yNLpelpH1NxxmC_6f4V8IBM="),
@@ -17,13 +18,12 @@ STRING_SESSION = [
 
 
 
+
+
 # User and Bot Configuration
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@YourBotUsername")
-ALLOWED_USER_ID = 6508157803  # Replace with your actual user ID
 OWNER_ID = "raoxc"  # Owner ID or username
 CHAT_ID = '-1002217780167'
-
-
 # Media and Version Info
 IPIC = "https://telegra.ph/file/6bb3994d5789d8e7f2c99.mp4"  # URL for the media file
 ubotversion = "1.0"  # Version of your userbot
