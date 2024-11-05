d3c369f1fc12465a7d136d12b1e903326f745476
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2024-11-05 13:41:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import random
+from os import getenv
+
+
+
+
+
+
+OWNER_ID = "raoxc"  # Owner ID or username
+# Media and Version Info
+IPIC = "https://telegra.ph/file/6bb3994d5789d8e7f2c99.mp4"  # URL for the media file
+ubotversion = "1.0"  # Version of your userbot
+USERNAME = "raoxc"
+GROUP_LINK = "https://t.me/raoxc_gc"
+UPIC = "https://envs.sh/_Cf.jpg"
+STARTPIC = "https://telegra.ph/file/cad7038fe82e47f79c609.jpg"
+OWNER_USERNAME = getenv("OWNER_USERNAME", "raoxc") 
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Dyoh_Ubot")
+
+
+
+
